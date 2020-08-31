f76e01f716cb702895642a5949818b710416b471
lukemorison <lukemorison@hotmail.com>
2020-08-31 22:06:54 +1000

    param list files etc.

---
+++ Steak_Render.R
@@ -0,0 +1,25 @@
+
+
+rmarkdown::render("steak_article_params.Rmd",
+                  params = list(region = "Mountain"))
+
+render_report <-function(regionvar){
+  template <-"steak_article_params.Rmd"
+  outfile <-sprintf("steakArticle_%s.html",regionvar)
+  parameters <-list(region = regionvar)
+  rmarkdown::render(template,
+                    output_file=outfile,
+                    params=parameters)
+  invisible(TRUE)
+}
+
+#render_report("Pacific")
+
+
+library(purrr)
+params_list <- list(list("East North Central",
+                         "East South Central", "Middle Atlantic",
+                         "Mountain", "New England", "Pacific",
+                         "South Atlantic", "West North Central",
+                         "West South Central"))
+purrr::pmap(params_list,render_report)
